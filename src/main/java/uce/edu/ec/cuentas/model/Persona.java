69f7bb70bcf8ad95fd43b98379db951474dedac9
d3rickArch <lspotify3022l@gmail.com>
2025-02-07 11:55:37 -0500

    ADDContent

---
+++ src/main/java/uce/edu/ec/cuentas/model/Persona.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uce.edu.ec.cuentas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author artur
+ */
+@Entity
+@Table(name = "persona")
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numCedula;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String direccion; 
+
+    public Long getNumCedula() {
+        return numCedula;
+    }
+
+    public void setNumCedula(Long numCedula) {
+        this.numCedula = numCedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
